1ee285889cfd6524d12f9fca6b53ac4619a5885e
Rostyslav Nikolaienko <r.nikolaienko@kwebbl.com>
2018-06-05 15:41:15 +0300

    section5

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Section1 from './Sections/Section1';
 import Section2 from './Sections/Section2';
 import Section3 from './Sections/Section3';
 import Section4 from './Sections/Section4';
+import Section5 from './Sections/Section5';
 
 import './Home.css';
 
@@ -11,4 +12,5 @@ export default () => (<div className="home">
   <Section2 />
   <Section3 />
   <Section4 />
+  <Section5 />
 </div>);
