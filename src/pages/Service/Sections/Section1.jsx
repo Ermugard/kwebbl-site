5b1804fc54c9be38520307020a2e36c817f7f9f6
Rostyslav Nikolaienko <r.nikolaienko@kwebbl.com>
2018-06-08 18:10:35 +0300

    section-1, section-2, content without styles

--- src/pages/Service/Sections/Section1.jsx
+++ src/pages/Service/Sections/Section1.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default () => (<div className="section_1">
   <div className="content">
-    section1
+    <h3>Growing with you</h3>
+    <h2>We can satisfy any appetite</h2>
+    <p>From Regional ISP to National Telecom Operator, we are ready and able to upgrade your service portfolio to the next level.</p>
+    <img src="" alt="" title="" />
   </div>
 </div>)
--- src/pages/Service/Sections/Section2.jsx
+++ src/pages/Service/Sections/Section2.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default () => (<div className="section_2">
   <div className="content">
-    section2
+    <div className="left">
+      {/* svg */}
+      <h3>Opex Pricing</h3>
+      <h2>No more huge
+        upfront expenses</h2>
+      <p>Kwebbl is a fully OPEX based service, we offer you all the required tools and services without requiring investment in expensive technical equipment and software.</p>
+      <p>You can start selling our multi-tenant “pay-as-you-go” communications solution right away.</p>
+    </div>
+    <img src="" alt="" title="" />
   </div>
 </div>)
