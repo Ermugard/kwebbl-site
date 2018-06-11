e9442412c10563a6d9845fa290f04a078896ccec
Rostyslav Nikolaienko <r.nikolaienko@kwebbl.com>
2018-06-11 15:22:04 +0300

    service section5-6

--- src/pages/Service/Sections/Section5.jsx
+++ src/pages/Service/Sections/Section5.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import imgSrc from '../../../public/images/service-section4.png';
+
 export default () => (<div className="section_5">
   <div className="content">
     <h2>One cloud platform
       for all your global customers</h2>
     <p>With Kwebbl you can scale your services across the world. Our private cloud and advanced localisation options give you the opportunity to provide world-wide service from one single cloud platform.</p>
-    {/*<img />*/}
+    <img src={imgSrc} alt="" title="" />
   </div>
 </div>)
--- src/pages/Service/Sections/Section6.jsx
+++ src/pages/Service/Sections/Section6.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
+import TagIcon from '../../../public/svgIcons/tag-icon.js';
+import img from '../../../public/images/service-section5.png';
+
 export default () => (<div className="section_6">
   <div className="content">
     <div className="left">
-      {/* svg */}
+      <TagIcon />
       <h2>Create added value
         integrations using our
         Realtime API</h2>
       <p>Our Realtime API can be used to integrate with all kinds of third party software, like CRMs or ERPs. It can also be used to develop additional software that caters to the wishes of your clients, whilst being fully integrated with the Kwebbl platform.</p>
     </div>
     <div className="right">
-      {/* img */}
+      <img src={img} alt="" title="" />
     </div>
   </div>
 </div>)
