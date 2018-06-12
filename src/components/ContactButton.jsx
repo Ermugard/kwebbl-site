d5bc66e2aeb3a189c6ef3b46745864bbdaa8e23a
Rostyslav Nikolaienko <r.nikolaienko@kwebbl.com>
2018-06-12 19:02:09 +0300

    styles

--- src/components/ContactButton.jsx
+++ src/components/ContactButton.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
-export default () => (<Link className="nav-link contact-us" to="/contactUs"><span>Contact Us</span></Link>);
+export default () => (<div className="contact-us">
+  <Link className="nav-link" to="/contactUs"><span>Contact Us</span></Link>
+</div>);
