6e6614e8462b0430ffa2abfc4afa1284ba9d0c41
Rostyslav Nikolaienko <r.nikolaienko@kwebbl.com>
2018-06-14 15:21:44 +0300

    fixed bug, whith scrollTop

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Route, Switch } from "react-router-dom";
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import {Route, Switch} from "react-router-dom";
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 import Home from '../pages/Home/Home';
 import Service from './Service/Service';
@@ -13,25 +13,37 @@ import ContactUs from '../pages/ContactUs/ContactUs';
 // import Vayama from '../pages/Stories/StoryList/Vayama';
 
 
-export default (props) => (
-  <div className={"page-layout"}>
-    <main className="main">
-      <TransitionGroup component="div" className={"transition-group"}>
-        <CSSTransition
-          key={props.location && props.location.key}
-          timeout={300}
-          classNames="fade"
-        >
-          <Switch location={props.location}>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/service" component={Service} />
-            <Route exact path="/distributors" component={Distributors} />
-            <Route exact path="/culture" component={Culture} />
-            <Route exact path="/stories" component={Stories} />
-            <Route exact path="/contactUs" component={ContactUs} />
-          </Switch>
-        </CSSTransition>
-      </TransitionGroup>
-    </main>
-  </div>
-);
+class PageLayout extends React.Component {
+
+  componentDidUpdate() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    const props = this.props;
+    return (
+      <div className={"page-layout"}>
+        <main className="main">
+          <TransitionGroup component="div" className={"transition-group"}>
+            <CSSTransition
+              key={props.location && props.location.key}
+              timeout={300}
+              classNames="fade"
+            >
+              <Switch location={props.location}>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/service" component={Service}/>
+                <Route exact path="/distributors" component={Distributors}/>
+                <Route exact path="/culture" component={Culture}/>
+                <Route exact path="/stories" component={Stories}/>
+                <Route exact path="/contactUs" component={ContactUs}/>
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        </main>
+      </div>
+    )
+  }
+};
+
+export default PageLayout;
